54f9483a5de763ec31b05739438c6b33c7a74bb6
MadBad <madbad82@gmail.com>
2014-12-07 12:17:23 +0100

    Fix typos in the generator script and rebuild the README.md

--- tools/readmegenerator.py
+++ tools/readmegenerator.py
@@ -1,9 +1,9 @@
-# this python script generate an html page
-# with all the cars and theyr prewievs
+# this python script generate (at the base of this repository) the README.md file
+# displayed by github
 
 import os
 import sys
-import shutil
+#import shutil
 import xml.etree.ElementTree as ET
 
 
@@ -35,52 +35,53 @@ for folder in carFolders:
 	carImg = '/cars/'+folder+'/'+folder+"-preview.jpg"
 	print carName +"("+folder+")";
 
-	#generate info table of the car
+	#get the category of the car from its xml file
 	category= root.findall("./section[@name='Car']/attstr[@name='category']")[0].attrib['val']
 
-	#insert the info table inside a category based array
+	#group cars data by category
 	if tmpout.has_key(category):
 		tmpout[category]+='';
 	else:
 		tmpout[category]='';
 
-	#tmpout[category]+= "\n!["+carName+"](/../master"+carImg+'?raw=true =150x"'+carName+'")'
+	#generate the img tag for the preview
 	tmpout[category]+= '<img src="/../master'+carImg+'" width="200px" alt="'+carName+'">'
 
-# create a table
+
+# generate the README.md content
 out= '''
-sdcars
+Speed Dreams Cars
 ======
 
-Unnoficial (usermade) Speed Dreams cars
+Unoficial (usermade) Speed Dreams car-mods.
 
 This package contains car-mods for the open source racing game [Speed Dreams](http://www.speed-dreams.org)
 
-We corrently count **''' 
+We currently count **''' 
 
 out+= str(carCount);
 
 out+= '''** playable cars in this package.
 
-[Download it now!](https://github.com/longtomjr/sdcars/archive/master.zip)
+**[Download it now!](https://github.com/longtomjr/sdcars/archive/master.zip)**
 
 
-Included cars mod previews
+Included car-mods previews
 ======
 
 '''
-# insert PREVIEWS
+# insert previously generated previews
 for category in tmpout:
 	out+=tmpout[category];
 
-# coontact informations
+# add contact informations
 out+= '''
 
 
 Contact informations
 ======
 
-If you wanto to add you mod to this repo, or for any info about it feel free to contact us!
+If you want to add your mod to this repo, or for any info about it feel free to contact us!
 
 Longtomjr - longtomjr [at] gmail.com
 
@@ -88,7 +89,7 @@ Madbad - madbad82 [at] gmail.com
 '''
 
 
-#save all into a file
+#save all into the file
 out_file = open("./../README.md","w")
 out_file.write(out)
 out_file.close()
